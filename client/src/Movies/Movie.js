ec0363607d86edd1f1282993b3f1548da36bf210
irisjitomo <irisjitomo@eyahoo.com>
2019-08-19 18:38:08 -0400

    finished MVP

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState();
  
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -42,7 +42,15 @@ const Movie = (props) => {
           Metascore: <strong>{metascore}</strong>
         </div>
         <h3>Actors</h3>
-
+        {!stars ? (
+  <h2>Loading stars..</h2>
+) : (
+  stars.map(star => (
+    <div key={star} className="movie-star">
+      {star}
+    </div>
+  ))
+)}
         {stars.map(star => (
           <div key={star} className="movie-star">
             {star}
